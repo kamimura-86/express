55f3a308490a178a3070e8572664db184a0d6926
上村蓉翼千 <kamimurayuuto@MacBook-Air.local>
2024-12-03 23:17:18 +0900

    add hello page

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var helloRouter = require('./routes/hello');
 var notesRouter = require('./routes/notes');
 
 var app = express();
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/hello', helloRouter);
 app.use('/notes', notesRouter);
 
 // catch 404 and forward to error handler
