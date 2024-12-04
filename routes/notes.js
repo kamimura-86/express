42a99b5e2d5235cbd1acca0887e6530ee9ff71c8
上村蓉翼千 <kamimurayuuto@MacBook-Air.local>
2024-12-04 21:21:45 +0900

    add yesno api

--- routes/notes.js
+++ routes/notes.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "*************";
+const uri = "mongodb+srv://2201100389cv:nrgXow92Xpwx4nJp@cluster0.ln1i6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
